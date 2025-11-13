fd2615de88ef2f0e25adfa6c27866d659a5f593e
Tanuj Jain <76273516+TANUJ0751@users.noreply.github.com>
2025-11-13 11:44:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import requests
 st.set_page_config(page_title="Weather Clothing Recommender", page_icon="👕")
 
 st.title("👕 Weather-Based Clothing Recommender")
-st.write("This App is created by Tanuj Jain")
+st.write("This App is created by Atul Singh Dhakad")
 # ---------- FUNCTION TO GET WEATHER ----------
 def get_weather(city):
     # Get coordinates for the city using Open-Meteo's geocoding API
